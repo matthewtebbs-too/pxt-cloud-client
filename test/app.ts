c38fe93127a9fdd9b7d2bfafcf9edee8a9d90997
Matthew Tebbs <matthew@muddytummy.com>
2018-06-07 12:53:37 -0700

    Tweak name of test function.

--- test/app.ts
+++ test/app.ts
@@ -12,9 +12,9 @@ import * as PxtCloud from '..';
 
 const debug = require('debug')('pxt-cloud:test');
 
-function test(api: WorldAPI) {
+function testWorldAPI(api: WorldAPI) {
     api.addUser({ name: 'Jilly Bean' }, debug);
     api.addUser({ name: 'Jilly Bean' }, debug);
 }
 
-new PxtCloud.WorldClient().connect().then((api: WorldAPI) => test(api)).catch(debug);
+new PxtCloud.WorldClient().connect().then((api: WorldAPI) => testWorldAPI(api)).catch(debug);
